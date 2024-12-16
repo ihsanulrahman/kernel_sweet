c05406041c55394edc5d4c8124c7c9eb308a34f4
simonpunk <simonpunk2016@gmail.com>
2024-12-16 14:38:50 +0800

    susfs4ksu: import 1.5.3
    head @ https://gitlab.com/simonpunk/susfs4ksu/-/commit/27009eacd97f2a0f8e4589a0a4d8448bd39b5bfe
    
    Signed-off-by: IHSAN UL RAHMAN <ihsanulrahman@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -69,6 +69,8 @@ static void proc_command_line_init(void) {
     }
 #endif
 }
+#ifdef CONFIG_KSU_SUSFS_SPOOF_PROC_CMDLINE
+extern int susfs_spoof_proc_cmdline(struct seq_file *m);
 #endif
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
@@ -76,6 +78,12 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 #ifdef ALTER_CMDLINE
 	seq_printf(m, "%s\n", proc_command_line);
 #else
+#ifdef CONFIG_KSU_SUSFS_SPOOF_PROC_CMDLINE
+	if (!susfs_spoof_proc_cmdline(m)) {
+		seq_putc(m, '\n');
+		return 0;
+	}
+#endif
 	seq_printf(m, "%s\n", saved_command_line);
 #endif
 	return 0;
