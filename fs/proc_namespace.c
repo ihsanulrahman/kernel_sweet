d0c4d359e4a748bcd37e90dcf148f7bc43b1db15
simonpunk <simonpunk2016@gmail.com>
2024-12-19 18:14:34 +0800

    susfs: next-241217: use susfs_is_current_ksu_domain() instead of ksu_is_ksu_domain()
    Signed-off-by: IHSAN UL RAHMAN <ihsanulrahman@gmail.com>

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -19,7 +19,7 @@
 #include "internal.h"
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-extern bool ksu_is_ksu_domain(void);
+extern bool susfs_is_current_ksu_domain(void);
 #endif
 
 static unsigned mounts_poll(struct file *file, poll_table *wait)
@@ -107,7 +107,7 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely((r->mnt.mnt_root->d_inode->i_state & 33554432) && !ksu_is_ksu_domain()))
+	if (unlikely((r->mnt.mnt_root->d_inode->i_state & 33554432) && !susfs_is_current_ksu_domain()))
 		return 0;
 #endif
 
@@ -148,7 +148,7 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely((r->mnt.mnt_root->d_inode->i_state & 33554432) && !ksu_is_ksu_domain()))
+	if (unlikely((r->mnt.mnt_root->d_inode->i_state & 33554432) && !susfs_is_current_ksu_domain()))
 		return 0;
 #endif
 
@@ -217,7 +217,7 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely((r->mnt.mnt_root->d_inode->i_state & 33554432) && !ksu_is_ksu_domain()))
+	if (unlikely((r->mnt.mnt_root->d_inode->i_state & 33554432) && !susfs_is_current_ksu_domain()))
 		return 0;
 #endif
 
